83567d362f0d604a0771fa8b54d42128644905ec
AW <aw@pop-os.localdomain>
2019-07-05 07:16:52 -0300

    build

--- build/index.js
+++ build/index.js
@@ -23,78 +23,187 @@ var Queue = function () {
         this.timer = null;
         this.block = 1;
         this.queue = [];
+        this.parsePromise = false;
     }
 
     _createClass(Queue, [{
         key: "push",
         value: function push(data) {
             this.queue.push(data);
+            return this;
         }
     }, {
         key: "setTimeout",
         value: function setTimeout(timer) {
             if (typeof timer == "number") this.timeout = timer;
+
+            return this;
         }
     }, {
         key: "setBlock",
         value: function setBlock(block) {
             if (typeof block == "number") this.block = block;
+
+            return this;
+        }
+    }, {
+        key: "parserReturnPromise",
+        value: function parserReturnPromise(returnPromise) {
+            if (typeof returnPromise == "boolean") this.parsePromise = returnPromise;
+
+            return this;
         }
     }, {
         key: "setParser",
-        value: function setParser(cb) {
-            var _this = this;
+        value: function () {
+            var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(cb) {
+                var _this = this;
 
-            this.timer = setInterval(_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
                 var data, key;
-                return regeneratorRuntime.wrap(function _callee$(_context) {
+                return regeneratorRuntime.wrap(function _callee2$(_context2) {
                     while (1) {
-                        switch (_context.prev = _context.next) {
+                        switch (_context2.prev = _context2.next) {
                             case 0:
-                                if (!(_this.queue.length > 0)) {
-                                    _context.next = 14;
+                                if (!this.parsePromise) {
+                                    _context2.next = 25;
+                                    break;
+                                }
+
+                                _context2.prev = 1;
+
+                                if (!(this.queue.length > 0)) {
+                                    _context2.next = 18;
                                     break;
                                 }
 
                                 data = [];
                                 key = 0;
 
-                            case 3:
-                                if (!(key < _this.block)) {
-                                    _context.next = 13;
+                            case 5:
+                                if (!(key < this.block)) {
+                                    _context2.next = 15;
                                     break;
                                 }
 
-                                if (!(_this.queue.length > 0)) {
-                                    _context.next = 9;
+                                if (!(this.queue.length > 0)) {
+                                    _context2.next = 11;
                                     break;
                                 }
 
-                                data.push(_this.queue[0]);
-                                _this.queue.shift();
-                                _context.next = 10;
+                                data.push(this.queue[0]);
+                                this.queue.shift();
+                                _context2.next = 12;
                                 break;
 
-                            case 9:
-                                return _context.abrupt("break", 13);
+                            case 11:
+                                return _context2.abrupt("break", 15);
 
-                            case 10:
+                            case 12:
                                 key++;
-                                _context.next = 3;
+                                _context2.next = 5;
                                 break;
 
-                            case 13:
+                            case 15:
+                                if (!(data.length > 0)) {
+                                    _context2.next = 18;
+                                    break;
+                                }
 
-                                if (data.length > 0) cb(data, _this.queue.length, new Date().getTime());
+                                _context2.next = 18;
+                                return cb(data, this.queue.length, new Date().getTime());
 
-                            case 14:
+                            case 18:
+                                _context2.next = 22;
+                                break;
+
+                            case 20:
+                                _context2.prev = 20;
+                                _context2.t0 = _context2["catch"](1);
+
+                            case 22:
+
+                                setTimeout(function (cb) {
+                                    _this.setParser(cb);
+                                }, this.timeout, cb);
+                                _context2.next = 26;
+                                break;
+
+                            case 25:
+                                this.timer = setInterval(_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+                                    var _data, _key;
+
+                                    return regeneratorRuntime.wrap(function _callee$(_context) {
+                                        while (1) {
+                                            switch (_context.prev = _context.next) {
+                                                case 0:
+                                                    _context.prev = 0;
+
+                                                    if (!(_this.queue.length > 0)) {
+                                                        _context.next = 15;
+                                                        break;
+                                                    }
+
+                                                    _data = [];
+                                                    _key = 0;
+
+                                                case 4:
+                                                    if (!(_key < _this.block)) {
+                                                        _context.next = 14;
+                                                        break;
+                                                    }
+
+                                                    if (!(_this.queue.length > 0)) {
+                                                        _context.next = 10;
+                                                        break;
+                                                    }
+
+                                                    _data.push(_this.queue[0]);
+                                                    _this.queue.shift();
+                                                    _context.next = 11;
+                                                    break;
+
+                                                case 10:
+                                                    return _context.abrupt("break", 14);
+
+                                                case 11:
+                                                    _key++;
+                                                    _context.next = 4;
+                                                    break;
+
+                                                case 14:
+
+                                                    if (_data.length > 0) cb(_data, _this.queue.length, new Date().getTime());
+
+                                                case 15:
+                                                    _context.next = 19;
+                                                    break;
+
+                                                case 17:
+                                                    _context.prev = 17;
+                                                    _context.t0 = _context["catch"](0);
+
+                                                case 19:
+                                                case "end":
+                                                    return _context.stop();
+                                            }
+                                        }
+                                    }, _callee, _this, [[0, 17]]);
+                                })), this.timeout);
+
+                            case 26:
                             case "end":
-                                return _context.stop();
+                                return _context2.stop();
                         }
                     }
-                }, _callee, _this);
-            })), this.timeout);
-        }
+                }, _callee2, this, [[1, 20]]);
+            }));
+
+            function setParser(_x) {
+                return _ref.apply(this, arguments);
+            }
+
+            return setParser;
+        }()
     }]);
 
     return Queue;
